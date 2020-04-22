e7331fad7336260b0c6e11684980b13f4c7e5c77
Allen Cheng <yl.allen.cheng@gmail.com>
2020-04-23 00:29:44 +0800

    Add listener and action to text input layout

--- app/src/main/java/com/ylallencheng/searchgithubusers/ui/search/SearchActivity.kt
+++ app/src/main/java/com/ylallencheng/searchgithubusers/ui/search/SearchActivity.kt
@@ -1,10 +1,20 @@
 package com.ylallencheng.searchgithubusers.ui.search
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.ylallencheng.searchgithubusers.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "SearchActivity"
+    }
 
     private lateinit var binding: ActivitySearchBinding
 
@@ -12,5 +22,22 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.textInputEditText.setOnEditorActionListener { v, actionId, _ ->
+            when (actionId) {
+                EditorInfo.IME_ACTION_SEARCH -> {
+                    val searchQuery = v.text
+                    Log.d(TAG, "search query is: $searchQuery")
+                }
+            }
+            finishEditing(this)
+            true
+        }
+    }
+
+    private fun finishEditing(activity: AppCompatActivity) {
+        val imm = activity.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        val currentFocusedView = currentFocus ?: View(activity)
+        imm.hideSoftInputFromWindow(currentFocusedView.windowToken, 0)
+        currentFocusedView.clearFocus()
     }
 }
